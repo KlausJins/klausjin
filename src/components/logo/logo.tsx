17cd3eed0ef26c78e1ff52f115050247086733a5
Klaus <klausjins@gmail.com>
2025-05-18 13:49:33 +0800

    fix: 优化Image组件缺少高度问题

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -4,18 +4,26 @@ import Image from 'next/image'
 
 interface ILogoPropsType {
   width?: number
+  height?: number
 }
 
 const Logo = (props: ILogoPropsType) => {
-  const { width = 32 } = props
+  const { width = 32, height = 32 } = props
   return (
     <div>
-      <Image className="dark:hidden" src={ImageAssests.LogoDark} alt={WEBSITE} width={width} />
+      <Image
+        className="dark:hidden"
+        src={ImageAssests.LogoDark}
+        alt={WEBSITE}
+        width={width}
+        height={height}
+      />
       <Image
         className="hidden dark:block"
         src={ImageAssests.LogoLight}
         alt={WEBSITE}
         width={width}
+        height={height}
       />
     </div>
   )
