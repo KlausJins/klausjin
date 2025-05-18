20938146e5f0b324586bb69e55d93485b90f1bfe
Klaus <klausjins@gmail.com>
2025-05-18 14:10:16 +0800

    fix: 修改ts的别名配置以及新增首页文字组件

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,3 +1,10 @@
+import Section from '@/features/home/section'
+
 export default function Home() {
-  return <div> KlausJin 个人博客网站</div>
+  return (
+    <div>
+      KlausJin 个人博客网站
+      <Section></Section>
+    </div>
+  )
 }
--- src/constants/info.ts
+++ src/constants/info.ts
@@ -8,5 +8,16 @@ export const WEBSITE = NICKNAME
 export const WEBSITE_DESCRIPTION =
   '🚀 一个用于记录自己成长的简单的个人博客网站，使用 Next.js 构建。'
 
+/** 个人 Github 地址 */
+export const GITHUB_PERSONAL_PAGE_LINK = 'https://github.com/KlausJins'
+
 /** 网站代码 Github 地址 */
 export const GITHUB_PAGE_LINK = 'https://github.com/KlausJins/klausjin'
+
+/** 首页文本 */
+export const HOME_PRE_TEXT = '你好，我是'
+export const HOME_NAME_TEXT = NICKNAME
+export const HOME_TPYE_ZH_TEXT = '一名前端开发工程师。'
+export const HOME_TPYE_EN_TEXT = 'A Web <Developer />.'
+export const HOME_GREETINIG_TEXT = '喜欢React、TypeScript \\owo/ ~'
+export const HOME_SLOGAN_TEXT = '我在这个网站记录我的成长，努力 💪 成为一个更好的程序员。'
