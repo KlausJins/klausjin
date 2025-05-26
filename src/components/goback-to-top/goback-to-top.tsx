7b7656a25143b50e650e004f3fe9dbe41018e5ad
Klaus <klausjins@gmail.com>
2025-05-26 13:52:24 +0800

    style: 适配手机端样式

--- src/components/goback-to-top/goback-to-top.tsx
+++ src/components/goback-to-top/goback-to-top.tsx
@@ -5,7 +5,11 @@ import Button from '@/components/ui/button'
 import { useScroll } from '@/hooks'
 import { clm } from '@/utils/normal'
 
-export const GoBackToTop = () => {
+interface IGoBackToTopProps {
+  className?: string
+}
+
+export const GoBackToTop = ({ className }: IGoBackToTopProps) => {
   const scrollDisstance = useScroll({ isThrottle: true })
 
   return (
@@ -13,7 +17,8 @@ export const GoBackToTop = () => {
       data-state={scrollDisstance >= 100 ? 'show' : 'unshow'}
       className={clm(
         'flex fixed bottom-8 right-8 z-10 data-[state=show]:animate-opacity-in data-[state=unshow]:animate-opacity-out',
-        scrollDisstance >= 100 ? 'opacity-100' : 'opacity-0 pointer-events-none'
+        scrollDisstance >= 100 ? 'opacity-100' : 'opacity-0 pointer-events-none',
+        className
       )}
       onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
     >
--- src/features/note/modal-detail.tsx
+++ src/features/note/modal-detail.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import IconSelf from '@/components/icons/icon-self'
+import { clm } from '@/utils/normal'
 import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 
@@ -29,17 +30,26 @@ const ModalDetail = () => {
       ></div>
 
       {/* 笔记弹窗 */}
-      <div className="relative container m-auto top-[50%] transform translate-y-[-50%] bg-lighterBgPrimary dark:bg-darkerBgPrimary w-[80vw] h-[92vh] rounded-3xl overflow-hidden px-6 py-6">
-        {/* 关闭按钮 */}
-        <div
-          className="absolute top-6 right-6 flex hover:cursor-pointer"
-          onClick={() => closePage()}
-        >
-          <IconSelf iconName="icon-[lucide--x]" />
+      <div
+        className={clm(
+          'relative container m-auto top-[50%] transform translate-y-[-50%] bg-lighterBgPrimary dark:bg-darkerBgPrimary ',
+          'w-[80vw] h-[92vh]  rounded-3xl overflow-auto px-6 py-6 pt-0',
+          'max-md:w-[100vw] max-md:h-[100vh] max-md:rounded-none'
+        )}
+      >
+        <div className="sticky top-0 flex justify-between items-center pt-4 pb-2 z-10 bg-lighterBgPrimary dark:bg-darkerBgPrimary">
+          {/* 布局占用 */}
+          <span></span>
+          {/* 默认标题 */}
+          <div className="text-xl font-black">笔记</div>
+          {/* 关闭按钮 */}
+          <div className="flex hover:cursor-pointer" onClick={() => closePage()}>
+            <IconSelf iconName="icon-[lucide--x]" />
+          </div>
         </div>
 
         {/* 笔记内容 */}
-        <div className="absolute">ModalDetail</div>
+        <div className=" relative h-[120vh] w-full">ModalDetail</div>
       </div>
     </div>
   )
