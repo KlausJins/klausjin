954bf4ec1d38cccba430820db5f087d1c1267d5b
Klaus <klausjins@gmail.com>
2025-05-27 23:51:32 +0800

    test: 测试样式

--- src/features/note/modal-detail.tsx
+++ src/features/note/modal-detail.tsx
@@ -11,52 +11,52 @@ const ModalDetail = () => {
   const modalRef = useRef<HTMLDivElement>(null)
   const overLayRef = useRef<HTMLDivElement>(null)
 
-  useEffect(() => {
-    const rect = JSON.parse(sessionStorage.getItem('modalRect') || 'null')
-    const modal = modalRef.current
-    const overLay = overLayRef.current
+  // useEffect(() => {
+  //   const rect = JSON.parse(sessionStorage.getItem('modalRect') || 'null')
+  //   const modal = modalRef.current
+  //   const overLay = overLayRef.current
 
-    if (overLay) {
-      overLay.style.opacity = '0'
+  //   if (overLay) {
+  //     overLay.style.opacity = '0'
 
-      requestAnimationFrame(() => {
-        overLay.style.transition = 'opacity 0.5s ease'
-        overLay.style.opacity = '1'
-      })
-    }
+  //     requestAnimationFrame(() => {
+  //       overLay.style.transition = 'opacity 0.5s ease'
+  //       overLay.style.opacity = '1'
+  //     })
+  //   }
 
-    if (rect && modal) {
-      const { top, left, width, height } = rect
-      addKeyframe(`
-        @keyframes opacity-in-111 {
-          60% {
-            opacity: 1;
-          }
-          100% {
-            opacity: 0;
-            color: transparent;
-          }
-        }`)
-      console.log('top, left, width, height: ', top, left, width, height)
-      modal.style.position = 'absolute'
-      modal.style.top = `${top}px`
-      modal.style.left = `${left}px`
-      modal.style.width = `${width}px`
-      modal.style.height = `${height}px`
-      // modal.style.opacity = '1'
-      modal.style.transform = 'none'
+  //   if (rect && modal) {
+  //     const { top, left, width, height } = rect
+  //     addKeyframe(`
+  //       @keyframes opacity-in-111 {
+  //         60% {
+  //           opacity: 1;
+  //         }
+  //         100% {
+  //           opacity: 0;
+  //           color: transparent;
+  //         }
+  //       }`)
+  //     console.log('top, left, width, height: ', top, left, width, height)
+  //     modal.style.position = 'absolute'
+  //     modal.style.top = `${top}px`
+  //     modal.style.left = `${left}px`
+  //     modal.style.width = `${width}px`
+  //     modal.style.height = `${height}px`
+  //     // modal.style.opacity = '1'
+  //     modal.style.transform = 'none'
 
-      requestAnimationFrame(() => {
-        modal.style.transition = 'all 0.5s ease'
-        modal.style.top = '50%'
-        modal.style.left = '50%'
-        modal.style.width = '80vw'
-        modal.style.height = '92vh'
-        // modal.style.opacity = '1'
-        modal.style.transform = 'translate(-50%, -50%)'
-      })
-    }
-  }, [])
+  //     requestAnimationFrame(() => {
+  //       modal.style.transition = 'all 0.5s ease'
+  //       // modal.style.top = '50%'
+  //       // modal.style.left = '50%'
+  //       modal.style.width = '80vw'
+  //       modal.style.height = '92vh'
+  //       // modal.style.opacity = '1'
+  //       // modal.style.transform = 'translate(-50%, -50%)'
+  //     })
+  //   }
+  // }, [])
 
   const closePage = () => {
     const modal = modalRef.current
@@ -115,13 +115,13 @@ const ModalDetail = () => {
           'absolute container m-auto bg-bgPrimary dark:bg-darkerBgPrimary',
           'w-[80vw] h-[92vh] rounded-3xl overflow-auto px-6 py-6 pt-0',
           'max-md:!w-[100vw] max-md:!h-[100vh] max-md:rounded-none',
-          'max-md:!top-[50%] will-change-transform'
+          'max-md:!top-0 will-change-transform'
         )}
         ref={modalRef}
       >
         <div className="sticky top-0 flex justify-center items-center pt-4 pb-2 z-10">
           {/* 默认标题 */}
-          <div className="text-xl font-black">笔记1</div>
+          <div className="text-xl font-black">笔记</div>
           {/* 关闭按钮 */}
           <div className="flex hover:cursor-pointer absolute right-0" onClick={() => closePage()}>
             <IconSelf iconName="icon-[lucide--x]" />
