85a76205b76ae19f780b3aec1aabe9f3ae7bad47
Klaus <klausjins@gmail.com>
2025-05-29 01:18:50 +0800

    factor: 重构modal的代码，将代码抽离至单独文件

--- src/features/note/modal-detail.tsx
+++ src/features/note/modal-detail.tsx
@@ -1,156 +1,35 @@
 'use client'
 
 import IconSelf from '@/components/icons/icon-self'
-import { addKeyframe, clm, removeBodyKeyframe } from '@/utils'
-import { useRouter } from 'next/navigation'
-import { useEffect, useRef, useState } from 'react'
+import { clm } from '@/utils'
+import { useRef } from 'react'
 import ReactDOM from 'react-dom'
 import { NoteInfo } from './note-info'
+import { useCloseModal, useIsMobile, useListenModalOpen, usePreventPageScroll } from './modal-hooks'
 
 const ModalDetail = () => {
-  const router = useRouter()
   const modalRef = useRef<HTMLDivElement>(null)
   const overLayRef = useRef<HTMLDivElement>(null)
 
-  // 判断是否为移动端
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 768)
+  // 监听判断是否为移动端
+  const { isMobile } = useIsMobile()
 
-  useEffect(() => {
-    const handleResize = () => {
-      const innerWidth = window.innerWidth
-      setIsMobile(innerWidth < 768)
-    }
+  // 监听 modal 的宽度大小状态，如果打开或者宽度大小改变则添加或运行动画
+  useListenModalOpen({
+    modalRef,
+    overLayRef,
+    isMobile
+  })
 
-    window.addEventListener('resize', handleResize)
+  // 关闭 modal 方法
+  const { closePage } = useCloseModal({
+    modalRef,
+    overLayRef,
+    isMobile
+  })
 
-    return () => {
-      window.removeEventListener('resize', handleResize)
-    }
-  }, [])
-
-  useEffect(() => {
-    const modal = modalRef.current
-    const overLay = overLayRef.current
-
-    if (!isMobile) {
-      // 网页端动画
-      const rect = JSON.parse(sessionStorage.getItem('modalRect') || 'null')
-
-      if (overLay) {
-        overLay.style.opacity = '0'
-
-        requestAnimationFrame(() => {
-          overLay.style.transition = 'opacity 0.5s ease'
-          overLay.style.opacity = '1'
-        })
-      }
-
-      if (rect && modal) {
-        const { top, left, width, height } = rect
-
-        addKeyframe(`
-        @keyframes card-close {
-          60% {
-            opacity: 1;
-          }
-          100% {
-            opacity: 0;
-            color: transparent;
-          }
-        }`)
-
-        modal.style.position = 'fixed'
-        modal.style.top = `${top}px`
-        modal.style.left = `${left}px`
-        modal.style.width = `${width}px`
-        modal.style.height = `${height}px`
-        modal.style.transform = 'none'
-
-        requestAnimationFrame(() => {
-          modal.style.transition = 'all 0.5s ease'
-          modal.style.top = '50%'
-          modal.style.left = '50%'
-          modal.style.width = '80vw'
-          modal.style.height = '92vh'
-          modal.style.transform = 'translate(-50%, -50%)'
-        })
-      }
-    } else {
-      // 移动端动画
-      if (modal && overLay) {
-        modal.style.transform = 'translate(-50%, 0)'
-        modal.style.left = '50%'
-        // modal.style.animation = 'opacity-in 0.3s ease'
-        // overLay.style.animation = 'opacity-in 0.3s ease'
-      }
-    }
-  }, [isMobile])
-
-  const closePage = () => {
-    const modal = modalRef.current
-    const overLay = overLayRef.current
-
-    if (!isMobile) {
-      const rect = JSON.parse(sessionStorage.getItem('modalRect') || 'null')
-
-      if (overLay) {
-        overLay.style.opacity = '0'
-      }
-
-      if (rect && modal) {
-        const { top, left, width, height } = rect
-        modal.style.animation = 'card-close 0.5s ease forwards'
-        modal.style.top = `${top}px`
-        modal.style.left = `${left}px`
-        modal.style.width = `${width}px`
-        modal.style.height = `${height}px`
-        modal.style.transform = 'none'
-
-        modal.addEventListener(
-          'transitionend',
-          () => {
-            router.back()
-            removeBodyKeyframe()
-          },
-          { once: true }
-        )
-      } else {
-        // 移动端动画
-        if (modal && overLay) {
-          modal.style.animation = 'opacity-out 0.3s ease'
-          overLay.style.animation = 'opacity-out 0.3s ease'
-
-          modal.addEventListener(
-            'animationend',
-            () => {
-              router.back()
-            },
-            { once: true }
-          )
-        }
-      }
-    } else {
-      router.back()
-      removeBodyKeyframe()
-    }
-  }
-
-  // 阻止滚动，并且防止页面闪烁
-  useEffect(() => {
-    const scrollY = +sessionStorage.getItem('scrollY')!
-    document.body.style.position = 'fixed'
-    document.body.style.top = `-${scrollY}px`
-    document.body.style.overflow = 'hidden'
-    document.body.style.width = '100%'
-
-    return () => {
-      document.body.style.position = ''
-      document.body.style.top = ''
-      document.body.style.overflow = ''
-      document.body.style.width = ''
-      window.scrollTo(0, scrollY)
-    }
-  }, [])
+  // 阻止页面来回滚动，并且防止页面闪烁
+  usePreventPageScroll()
 
   return (
     <>
