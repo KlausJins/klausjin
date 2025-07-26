e90c0178b9129c74adf3a5fc561e0275e91e8d7f
Klaus <klausjins@gmail.com>
2025-07-26 16:08:53 +0800

    fix: 修复OSS的配置

--- src/utils/oss.ts
+++ src/utils/oss.ts
@@ -13,7 +13,6 @@ export const uploadFile = async (file: File) => {
   console.log(file)
 
   try {
-    const OSSclient = await aliOSS()
     const arrayBuffer = await file.arrayBuffer()
     const buffer = Buffer.from(arrayBuffer)
 
@@ -23,7 +22,7 @@ export const uploadFile = async (file: File) => {
 
     // 填写OSS文件完整路径和本地文件的完整路径。OSS文件完整路径中不能包含Bucket名称。
     // 如果本地文件的完整路径中未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件。
-    const result = await OSSclient.put(filename, buffer)
+    const result = await aliOSS.put(filename, buffer)
     console.log(result)
   } catch (err) {
     console.log(err)
