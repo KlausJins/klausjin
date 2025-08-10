95b817d5a23405a3d74507a61f274172be5f43fd
Klaus <klausjins@gmail.com>
2025-08-11 00:16:53 +0800

    feat: 添加了百度统计功能

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,16 @@ import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import { HerouiProviders } from './providers'
 import { Console } from '@/components/console-text'
+import Script from 'next/script'
 
 export default function RootLayout({
   children
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const siteId = process.env.NEXT_PUBLIC_BAIDU_SITE_ID
+  const siteName = process.env.NEXT_PUBLIC_SITE_NAME
+
   return (
     <html lang="en" suppressHydrationWarning>
       <meta
@@ -23,6 +27,28 @@ export default function RootLayout({
             <Console />
           </NextThemeProvider>
         </HerouiProviders>
+
+        {/* 百度统计 */}
+        {siteId && (
+          <Script
+            id="baidu-tongji"
+            // 确保脚本在页面 hydration 之后加载
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                if (window.location.hostname.endsWith('${siteName}')) {
+                  var _hmt = _hmt || [];
+                  (function() {
+                    var hm = document.createElement("script");
+                    hm.src = "https://hm.baidu.com/hm.js?${siteId}";
+                    var s = document.getElementsByTagName("script")[0];
+                    s.parentNode.insertBefore(hm, s);
+                  })();
+                }
+              `
+            }}
+          />
+        )}
       </body>
     </html>
   )
